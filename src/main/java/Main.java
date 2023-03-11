ca08c3d6a90898d8d153cc8f78d27f1eee3cc1e9
Leonid Sitnikov <hedgehog@yandex.ru>
2023-03-11 12:06:07 +0300

    added TODO comment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,17 @@
 import java.io.File;
+import java.util.function.ToDoubleBiFunction;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
         System.out.println(engine.search("data"));
+        /*TODO
+        * здесь создайте сервер, который отвечал бы на нужные запросы
+        * слушать он должен порт 8989
+        * отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
+        **/
 
-        // здесь создайте сервер, который отвечал бы на нужные запросы
-        // слушать он должен порт 8989
-        // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
     }
 }
+
+//TODO Создать класс клиента для работы с сервером
